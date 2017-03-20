6d0c3491f86b282f801dc49ca9c76596756dbaf8
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-03-20 15:43:20 -0400

    Add factory_girl_rails and a users factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user, do
+    first_name "Tom"
+    last_name "Hanks"
+    username "forest_gump"
+    email "hank_da_man@castaway.com"
+    password "passsword"
+  end
+end
