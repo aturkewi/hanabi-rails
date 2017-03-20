37339f2a07c1dea855b3d84ef9d8fab6dc96ac93
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-03-20 15:33:47 -0400

    Add some login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    post '/signup', to: "users#signup"
+    post '/login', to: "auth#login"
+    post '/auth/referesh', to: "auth#refresh"
+  end
+
 end
