7118315575f490c5caa97b4f5e009484efc17cff
Luke Ghenco <lukeghenco@gmail.com>
2017-04-01 22:18:36 -0400

    create user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe 'validations' do 
+
+    it 'requires a username, email, and password upon creation' do 
+      user = build(:user, username: nil, email: nil, password: nil)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq(["Password can't be blank", "Email can't be blank", "Username can't be blank"])
+    end 
+
+    it 'requirs that a username and email are unique' do 
+      create(:user)
+      user = build(:user)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq(["Email has already been taken", "Username has already been taken"])
+    end
+  end
 end
