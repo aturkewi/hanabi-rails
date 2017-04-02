fc243fadaebb318da8402b69efb71a79f64e4789
Luke Ghenco <lukeghenco@gmail.com>
2017-04-02 16:44:51 -0400

    connect App container to redux and add Flow

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,24 +1,45 @@
+// @flow 
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router, 
   Switch
 } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { logout } from '../redux/modules/Auth/actions';
 import Signup from '../views/Signup';
+import Navbar from '../components/Navbar';
+
+type Props = {
+  isAuthenticated: boolean,
+  logout: () => void,
+}
 
 class App extends Component {
 
+  props: Props
+
   render() {
+    const { isAuthenticated, logout } = this.props;
+
     return (
       <Router>
-        <div className="app">
-          <Switch>
-            <Signup />
-            
-          </Switch>
-        </div>
+        <Container>
+          <div className="app">
+            <Navbar isAuthenticated={isAuthenticated} logout={logout} />
+            <Switch>
+              <Signup />
+              
+            </Switch>
+          </div>
+        </Container>
       </Router>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+  }), { logout }
+)(App);
