d88e44adc60acfbc065f89d443f8f06ff43da667
Luke Ghenco <lukeghenco@gmail.com>
2017-04-01 21:22:17 -0400

    built spec for POST /auth

--- spec/requests/api/auth_request_spec.rb
+++ spec/requests/api/auth_request_spec.rb
@@ -4,24 +4,50 @@ RSpec.describe "Users API", type: :request do
 
   describe "POST /auth" do 
 
-    it "logs in a user and returns the user info with a JWT token" do 
-      params = {
-        user: {
-          username: "billy",
-          password: "password"
+    describe "on success" do 
+
+      it "logs in a user and returns the user info with a JWT token" do 
+        user = create(:user)
+        params = {
+          user: {
+            username: user.username,
+            password: user.password
+          }
         }
-      }
 
-      post '/api/auth',
-        params: params.to_json,
-        headers: { 'Content-Type': 'application/json' }
+        post '/api/auth',
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
 
-      body = JSON.parse(response.body)
-     
-      expect(response.status).to eq(200)
-      expect(body['token']).not_to eq(nil)
-      expect(body['user']["id"]).not_to eq(nil)
-      expect(body['password_digets']).to eq(nil)
-    end 
+        body = JSON.parse(response.body)
+      
+        expect(response.status).to eq(200)
+        expect(body['token']).not_to eq(nil)
+        expect(body['user']["id"]).not_to eq(nil)
+        expect(body['password_digets']).to eq(nil)
+      end 
+    end
+
+    describe "on failure" do 
+
+      it "returns an error message" do 
+        user = build(:user)
+        params = {
+          user: {
+            username: user.username,
+            password: user.password
+          }
+        }
+
+        post '/api/auth',
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+
+        body = JSON.parse(response.body)
+      
+        expect(response.status).to eq(500)
+        expect(body['errors'][0]['message']).to eq('Incorrect username or password')
+      end 
+    end
   end
 end
