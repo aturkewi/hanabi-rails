8c4ac2b713f7a69c29baffa182f7625cba28a025
Luke Ghenco <lukeghenco@gmail.com>
2017-04-01 21:22:26 -0400

    passing tests for POST /auth

---
+++ app/controllers/api/auth_controller.rb
@@ -0,0 +1,17 @@
+class Api::AuthController < ApplicationController
+
+  def login
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      render json: { 
+        token: Auth.create_token(user), 
+        user: user.attributes.except("password_digest") 
+      }
+    else
+      render json: { 
+        errors: [ { message: 'Incorrect username or password' } ]
+      }, status: 500
+    end
+  end
+
+end
