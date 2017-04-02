10893e8ae3afc726958286bbfafad92b9e19d65f
Luke Ghenco <lukeghenco@gmail.com>
2017-04-01 23:01:21 -0400

    add Router and Switch to App.js

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Redirect
+} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        App Container 
-      </div>
+      <Router>
+        <div className="app">
+          <Switch>
+            
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
