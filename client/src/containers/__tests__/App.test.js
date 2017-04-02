9bf898baf3f569c70c8b38b8b7a0ae59be0e6322
Luke Ghenco <lukeghenco@gmail.com>
2017-04-01 23:01:10 -0400

    create App.js tests

---
+++ client/src/containers/__tests__/App.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { shallow, mount } from 'enzyme';
+import App from '../App';
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
+let store = mockStore()
+let wrapper; 
+
+describe('App', () => {
+  
+  beforeEach(() => {
+    wrapper = mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  })
+
+  it('renders without crashing', () => {
+    expect(wrapper).toBeDefined();
+  })
+
+  it('wraps content in a div with .app class', () => {
+    expect(wrapper.find('.app').length).toEqual(1);
+  })
+
+  it('contains a Router component', () => {
+    expect(wrapper.find('Router').length).toEqual(1);
+  })
+
+  it('contains a Switch component', () => {
+    expect(wrapper.find('Switch').length).toEqual(1);
+  })
+})
