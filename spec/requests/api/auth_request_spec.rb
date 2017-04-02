c1aebfc0639d6c4db3047c5b90a4571956b40724
Luke Ghenco <lukeghenco@gmail.com>
2017-04-01 21:32:05 -0400

    add tests for POST /auth/refresh

--- spec/requests/api/auth_request_spec.rb
+++ spec/requests/api/auth_request_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Users API", type: :request do
         expect(response.status).to eq(200)
         expect(body['token']).not_to eq(nil)
         expect(body['user']["id"]).not_to eq(nil)
-        expect(body['password_digets']).to eq(nil)
+        expect(body['password_digest']).to eq(nil)
       end 
     end
 
@@ -47,6 +47,49 @@ RSpec.describe "Users API", type: :request do
       
         expect(response.status).to eq(500)
         expect(body['errors'][0]['message']).to eq('Incorrect username or password')
+      end 
+    end
+  end
+
+  describe 'POST /auth/refresh' do 
+
+    describe 'on success' do 
+      it "returns the user info with a new JWT token" do 
+
+        # binding.pry
+        user = create(:user)
+        token = Auth.create_token(user.id)
+        
+        post '/api/auth/refresh',
+          headers: { 
+            'Content-Type': 'application/json', 
+            'Authorization': "Bearer #{token}"
+          }
+
+        body = JSON.parse(response.body)
+        binding.pry
+      
+        expect(response.status).to eq(200)
+        expect(body['token']).not_to eq(token)
+        expect(body['token']).not_to eq(nil)
+        expect(body['user']["id"]).to eq(user.id)
+      end 
+    end 
+
+    describe 'on failure' do 
+      it 'returns an error message' do 
+        token = 'abc.123.def.456'
+
+        post '/api/auth/refresh',
+          headers: { 
+            'Content-Type': 'application/json', 
+            'Authorization': "Bearer #{token}"
+          }
+
+        body = JSON.parse(response.body)
+      
+        expect(response.status).to eq(500)
+        expect(body['errors'][0]['message']).to eq('Token is invalid')
       end 
     end
   end
