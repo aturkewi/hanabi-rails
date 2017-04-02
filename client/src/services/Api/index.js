d4be6c423c4ae8a582ad4123d4e28ed0f43d9d9d
Luke Ghenco <lukeghenco@gmail.com>
2017-04-01 23:27:06 -0400

    add export modules for .fetch, .post, .patch & .delete

--- client/src/services/Api/index.js
+++ client/src/services/Api/index.js
@@ -27,3 +27,48 @@ export const queryString = (params: Object) => {
 
   return `${query.length ? '?' : ''}${query}`;
 }
+
+export default {
+
+  fetch(url: string, params: Object = {}) {
+    return fetch(`${API}${url}${queryString(params)}`, {
+        method: 'GET', 
+        headers: headers(),
+      })
+      .then(parseResponse)
+      .catch(console.log);
+  },
+  
+  post(url: string, data: Object = {}) {
+    const body: string = JSON.stringify(data);
+
+    return fetch(`${API}${url}`, {
+      method: 'POST',
+      headers: headers(),
+      body,
+    })
+    .then(parseResponse)
+    .catch(console.log);
+  },
+
+  patch(url: string, data: Object) {
+    const body: string = JSON.stringify(data);
+
+    return fetch(`${API}${url}`, {
+      method: 'PATCH', 
+      headers: headers(),
+      body,
+    })
+    .then(parseResponse)
+    .catch(console.log);
+  }, 
+
+  delete(url: string) {
+    return fetch(`${API}${url}`, {
+      method: 'DELETE', 
+      headers: headers(),
+    })
+    .then(parseResponse)
+    .catch(console.log);
+  }
+}
