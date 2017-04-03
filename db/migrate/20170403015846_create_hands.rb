0cd3c227934f40ffc074710d00bac6e130283c09
Luke Ghenco <lukeghenco@gmail.com>
2017-04-02 21:59:21 -0400

    add migration for Hand model

---
+++ db/migrate/20170403015846_create_hands.rb
@@ -0,0 +1,10 @@
+class CreateHands < ActiveRecord::Migration[5.0]
+  def change
+    create_table :hands do |t|
+      t.references :user, foreign_key: true
+      t.references :game, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403012349) do
+ActiveRecord::Schema.define(version: 20170403015846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20170403012349) do
     t.string   "title",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "hands", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_hands_on_game_id", using: :btree
+    t.index ["user_id"], name: "index_hands_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +40,6 @@ ActiveRecord::Schema.define(version: 20170403012349) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "hands", "games"
+  add_foreign_key "hands", "users"
 end
