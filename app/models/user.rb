3a433c411fb9ef0f16e2c3f6dbaa38410f94d1c4
Luke Ghenco <lukeghenco@gmail.com>
2017-04-02 22:45:37 -0400

    add relationships to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_many :hands
+  has_many :games, through: :hands
+  has_many :game_cards, through: :hands
 
+  has_secure_password
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
 end
