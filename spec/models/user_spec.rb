7980c1b95afb301921d3e634cc17ea42513f649e
Luke Ghenco <lukeghenco@gmail.com>
2017-04-02 21:57:01 -0400

    add relationship tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to eq(["Email has already been taken", "Username has already been taken"])
     end
   end
+
+  describe 'relationships' do 
+
+    it 'has many hands' do 
+      game = create(:game)
+      user = create(:user)
+      user.hands.create(:hand, game: game)
+      
+      expect(user.hands.count).to eq(1)
+      expect(user.hands.first.id).not_to eq(nil)
+    end
+
+    it 'has many games through hands'
+
+    it 'has many game_cards through hands' do 
+      user = create(:user)
+      user.games.create(:game)
+      
+      expect(user.games.count).to eq(1)
+      expect(user.games.first.id).not_to eq(nil)
+    end
+  end
 end
