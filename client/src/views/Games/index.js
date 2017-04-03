aa4ec06fb7aa6c17c229bcb68f2b78fcc5d541c7
Luke Ghenco <lukeghenco@gmail.com>
2017-04-02 21:28:51 -0400

    add basic Games Component

---
+++ client/src/views/Games/index.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+
+class Games extends Component { 
+
+  render() {
+    return (
+      <div className="games">
+        Games
+      </div>
+    );
+  }
+}
+
+export default Games;
