6229a03524b7e8adce53b9315ef380ae0afa6e62
Luke Ghenco <lukeghenco@gmail.com>
2017-04-04 06:49:50 -0400

    add enum status options in Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,8 @@ class Game < ApplicationRecord
   has_many :users, through: :hands
   has_many :game_cards, dependent: :destroy
 
+  enum status: [:setup, :active, :completed]
+
   validates :title, presence: true
   validates :title, uniqueness: true
 
