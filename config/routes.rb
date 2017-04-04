c780336931270a11e6e1b6be5210301b267ceb4e
Luke Ghenco <lukeghenco@gmail.com>
2017-04-04 05:30:21 -0400

    add resources :games to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
   namespace :api do
+    # /api/users
     resources :users, only: [:create]
+
+    # /api/auth
     post '/auth', to: "auth#login"
     post '/auth/refresh', to: "auth#refresh"
+
+    # /api/games
+    resources :games
   end
 
 end
