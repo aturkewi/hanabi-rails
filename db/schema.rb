6497d0e3498108f97bcd2b4127ef07e08b50c43a
Luke Ghenco <lukeghenco@gmail.com>
2017-04-04 06:49:27 -0400

    add status enumerator to Game model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403022257) do
+ActiveRecord::Schema.define(version: 20170404094815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(version: 20170403022257) do
   end
 
   create_table "games", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",                  null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "status",     default: 0, null: false
   end
 
   create_table "hands", force: :cascade do |t|
