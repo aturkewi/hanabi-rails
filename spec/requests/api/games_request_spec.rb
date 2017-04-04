063e46c8a0f0fa25b0a416bc1c88d2ee7e56a0bb
Luke Ghenco <lukeghenco@gmail.com>
2017-04-04 05:38:55 -0400

    add tests to validate all /api/games routes require a token

--- spec/requests/api/games_request_spec.rb
+++ spec/requests/api/games_request_spec.rb
@@ -12,26 +12,39 @@ RSpec.describe "Games API", type: :request do
     }
     @tokenless_headers = { "CONTENT_TYPE" => "application/json" }
   end
+
+  it 'all routes require a token' do 
+      responses = []
+      response_bodies = []
+      game = create(:game)
+      params = { game: { title: "Test Game" } }
+
+      post '/api/games', params: params.to_json, headers: @tokenless_headers
+      responses << response 
+      response_bodies << JSON.parse(response.body)
+
+      get "/api/games", headers: @tokenless_headers
+      responses << response 
+      response_bodies << JSON.parse(response.body)
+      
+      get "/api/games/#{game.id}", headers: @tokenless_headers
+      responses << response 
+      response_bodies << JSON.parse(response.body)
+    
+      responses.each { |r| expect(r).to have_http_status(403) }
+      response_bodies.each { |body| expect(body["errors"]).to eq([{ "message" => "You must include a JWT Token!" }]) }
+    end
     
   describe 'POST /games' do 
 
-    it 'requires a token' do 
-      params = { game: { title: "Test Game" } }
-      post '/api/games', params: params.to_json, headers: @tokenless_headers
-      body = JSON.parse(response.body).with_indifferent_access
     
-      expect(response).to have_http_status(403)
-      expect(body["errors"]).to eq([
-        { "message" => "You must include a JWT Token!" }
-      ])
-    end
 
     describe 'success' do 
 
       it 'returns the newly created game' do     
         params = { game: { title: "Test Game" } }
         post '/api/games', params: params.to_json, headers: @token_headers
-        body = JSON.parse(response.body).with_indifferent_access
+        body = JSON.parse(response.body)
 
         expect(response.content_type).to eq("application/json")
         expect(response).to have_http_status(200)
@@ -48,7 +61,7 @@ RSpec.describe "Games API", type: :request do
         game = create(:game)
         params = { game: { title: game.title } }
         post '/api/games', params: params.to_json, headers: @token_headers
-        body = JSON.parse(response.body).with_indifferent_access
+        body = JSON.parse(response.body)
 
         expect(response.content_type).to eq("application/json")
         expect(response).to have_http_status(500)
