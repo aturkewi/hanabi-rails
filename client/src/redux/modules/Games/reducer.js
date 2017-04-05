6a4f03bd8e25f28edd4e2f8b228e633e2e18787e
Luke Ghenco <lukeghenco@gmail.com>
2017-04-05 07:56:29 -0400

    add reducer to Games Module

---
+++ client/src/redux/modules/Games/reducer.js
@@ -0,0 +1,17 @@
+export default (state = [], action) {
+
+  switch(action.type) {
+
+    case 'SET_GAMES':
+      return action.games;
+
+    case 'ADD_GAME':
+      return [
+        ...state, 
+        action.game
+      ];
+
+    default: 
+      return state;
+  }
+}
