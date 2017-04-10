fe2d76757cd1908616dfbbbc8ef845af5922fb43
Luke Ghenco <lukeghenco@gmail.com>
2017-04-10 14:54:40 -0400

    add GameChannel

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,10 +1,26 @@
 class GameChannel < ApplicationCable::Channel
 
   def subscribed
-    stream_from Game.all
+    stream_from "game_channel_for_current_user_id#{current_user.id}"
   end
 
   def unsubscribed
   end
+
+  def speak(data)
+    Game.create!(title: data['title'])
+  end
+
+  def get_games 
+    games = Game.all
+    ActionCable.server.broadcast('game_channel', games: render_games(games))
+  end
+
+  private 
+
+    def render_games(games)
+      ApplicationController.renderer.render(partial: "games/index.json.jbuilder", locals: { games: games })
+    end
+
   
 end
