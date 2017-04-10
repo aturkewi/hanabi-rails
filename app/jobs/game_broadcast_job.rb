c5da1947c4844cac092183df1a9c0fb6a88583af
Luke Ghenco <lukeghenco@gmail.com>
2017-04-10 14:55:12 -0400

    add GameBroadcast

--- app/jobs/game_broadcast_job.rb
+++ app/jobs/game_broadcast_job.rb
@@ -1,3 +1,12 @@
-class GameBroadcastJob < ApplicationJob 
-  
+class GameBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(game)
+    ActionCable.server.broadcast('game_channel', game: render_game(game))
+  end
+
+  private
+    def render_game(game)
+      ApplicationController.renderer.render(partial: "games/game.json.jbuilder", locals: { game: game })
+    end
 end
