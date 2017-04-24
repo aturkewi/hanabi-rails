63bc80edac4d53d16654ab9749d4f36db5c71ec9
Luke Ghenco <lukeghenco@gmail.com>
2017-04-24 15:53:37 -0400

    Remove Game component code from Games component

--- client/src/views/Games/index.js
+++ client/src/views/Games/index.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import cable from '../../services/Cable';
 
-class Game extends Component {
+class Games extends Component { 
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+
     this.state = {
       inputValue: ''
     }
   }
   
+  componentDidMount() {
+    this.subscription = cable.subscriptions.create('GameChannel', {
+
+      connected() { 
+        console.log('connected: action cable')
+      },
+      
+      received(data) {
+        console.log('I have received the data')
+        if (data.games) {
+          console.log(JSON.parse(data.games))
+        } else if (data.game) {
+          console.log(JSON.parse(data.game))
+        } else if (data.errors) {
+          console.log(JSON.parse(data.errors))
+        }
+      },
+
+      speak(title) {
+        return this.perform('speak', { title: title });
+      },
+
+      getGames() {
+        return this.perform('get_games', null);
+      },
+
+      disconnected() { 
+        console.log("disconnected: action cable" )
+      }
+    })
+    
+    setTimeout(() => {
+      this.subscription.getGames();
+    }, 1000) 
+  }
+  
+  componentWillUnmount() {
+    this.subscription && cable.subscriptions.remove(this.subscription);
+  }
+
   handleOnSubmit = (event) => {
     event.preventDefault()
-    this.props.createGame(this.state.inputValue)
+    this.subscription.speak(this.state.inputValue);
     this.setState({
       inputValue: ''
     })
@@ -25,61 +66,14 @@ class Game extends Component {
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleOnSubmit}> 
-        <input onChange={(event) => this.handleOnChange(event)} value={this.state.inputValue} />
-      </form>
-    )
-      
-  }
 
-}
-  
-
-class Games extends Component { 
-  
-  componentDidMount() {
-    this.subscription = cable.subscriptions.create({ channel: 'GameChannel', game_id: 1 }, {
-
-      connected() { 
-        console.log('connected: action cable')
-      },
-      
-      received(data) {
-        console.log('I have received the data')
-        console.log(JSON.parse(data.game))
-      },
-
-      speak(title) {
-        return this.perform('speak', { title: title });
-      },
-
-      getGame() {
-        return this.perform('get_game', null);
-      },
-
-      disconnected() { 
-        console.log("disconnected: action cable" )
-      }
-    })
-    
-    setTimeout(() => {
-      this.subscription.getGame();
-    }, 1000) 
-  }
-  
-  componentWillUnmount() {
-    this.subscription && cable.subscriptions.remove(this.subscription)
-  }
-  
-  createGame = (title) => this.subscription.speak(title);
-
-  render() {
     return(
       <div>
         <button onClick={this.getGames}>Get Games</button>
         <h1>Games Channel</h1>
-         <Game createGame={this.createGame} />
+        <form onSubmit={this.handleOnSubmit}> 
+          <input onChange={(event) => this.handleOnChange(event)} value={this.state.inputValue} />
+        </form>
       </div>
     )
   }
