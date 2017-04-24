80624a89048269386686e9ce5d9fde4759b2faad
Luke Ghenco <lukeghenco@gmail.com>
2017-04-24 15:52:02 -0400

    Add get_game, render_games and render_errors to GameChannel

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,26 +1,41 @@
 class GameChannel < ApplicationCable::Channel
 
   def subscribed
-    stream_from "game_channel_#{params[:game_id]}"
+    stream_from "game_channel"
   end
 
   def unsubscribed
   end
 
   def speak(data)
-    Game.create!(title: data['title'])
+    game = Game.new(title: data['title'])
+    if !game.save 
+      ActionCable.server.broadcast('game_channel', errors: render_errors(game.errors.messages))
+    end
   end
 
-  def get_game
-    game = Game.find_by(id: params[:game_id])
-    ActionCable.server.broadcast("game_channel_#{params[:game_id]}", game: render_game(game))
+  def get_games
+    games = Game.all
+    ActionCable.server.broadcast('game_channel', games: render_games(games))
+  end
+
+  def get_game 
+    game = Game.find_by(id: params[:id])
+    ActionCable.server.broadcast('game_channel', game: render_game(game))
   end
 
   private 
+
+    def render_games(games)
+      ApplicationController.renderer.render(partial: "games/games.json.jbuilder", locals: { games: games })
+    end
 
     def render_game(game)
       ApplicationController.renderer.render(partial: "games/game.json.jbuilder", locals: { game: game })
     end
 
+    def render_errors(errors) 
+      ApplicationController.renderer.render(json: { errors: errors })
+    end
   
 end
