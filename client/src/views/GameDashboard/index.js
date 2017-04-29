bf253347cac407a380dc30802a2b566465a77aad
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-04-29 16:17:26 -0400

    Set game state and open socket on game page

--- client/src/views/GameDashboard/index.js
+++ client/src/views/GameDashboard/index.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
+import cable from '../../services/Cable';
 
 class GameDashboard extends Component {
+
+  componentDidMount() {
+    const self = this;
+    this.subscription = cable.subscriptions.create('GameChannel', {
+
+      connected() { 
+        console.log('connected: action cable')
+      },
+      
+      received(data) {
+        console.log('I have received the data')
+        if (data.game) {
+          var game = JSON.parse(data.game)
+          self.setState({
+            game
+          })
+        } else if (data.game) {
+          console.log(JSON.parse(data.game))
+        } else if (data.errors) {
+          console.log(JSON.parse(data.errors))
+        }
+      },
+      
+      getGame() {
+        const id = self.props.match.params.slug
+        return this.perform('get_game', { id: id });
+      },
+
+      disconnected() { 
+        console.log("disconnected: action cable" )
+      }
+    })
+    
+    setTimeout(() => {
+      this.subscription.getGame();
+    }, 1000) 
+  }
 
   render() {
     return (
