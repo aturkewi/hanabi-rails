f99ab8890d51fa8bd4f0bab0fb08aa69269146bb
Luke Ghenco <lukeghenco@gmail.com>
2017-05-18 14:33:05 -0400

    Update GamesSubscription reducer to handle creation & activation

--- client/src/redux/modules/GamesSubscription/reducer.js
+++ client/src/redux/modules/GamesSubscription/reducer.js
@@ -1,7 +1,22 @@
-const initialState = {}
+const initialState = {
+  creatingSubscription: false,
+  activeSubscription: false
+}
 
 export default (state = initialState, action) => {
   switch(action.type) {
+
+    case 'CREATING_SUBSCRIPTION':
+      return {
+        creatingSubscription: true,
+        activeSubscription: false
+      };
+
+    case 'ACTIVATE_SUBSCRIPTION': 
+      return {
+        creatingSubscription: false,
+        activeSubscription: true
+      }
     
     default: 
       return state;
