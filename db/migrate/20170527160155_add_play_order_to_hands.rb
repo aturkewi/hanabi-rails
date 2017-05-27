575902213934ae8d187ab47baf337285abbee89a
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-05-27 12:18:05 -0400

    Psych! Remove play order from hands

--- db/migrate/20170527160155_add_play_order_to_hands.rb
+++
@@ -1,5 +0,0 @@
-class AddPlayOrderToHands < ActiveRecord::Migration[5.0]
-  def change
-    add_column :hands, :play_order, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527160155) do
+ActiveRecord::Schema.define(version: 20170515191045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20170527160155) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "play_posotion"
-    t.integer  "play_order"
     t.index ["game_id"], name: "index_hands_on_game_id", using: :btree
     t.index ["user_id"], name: "index_hands_on_user_id", using: :btree
   end
