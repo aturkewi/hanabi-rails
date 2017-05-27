f3ed38b94bb8938e896f95ad10668b625f23ae38
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-05-27 16:10:06 -0400

    Display cards, misses, and clues for active game

--- client/src/views/GameDashboard/ActiveGame.js
+++ client/src/views/GameDashboard/ActiveGame.js
@@ -4,6 +4,18 @@ const ActiveGame = (props) => {
   return(
     <div className='active-game'>
       <div>
+        <h2>The state of affairs</h2>
+        <ul>
+          <li>
+            Cards left in the deck: { props.game.deck.length }
+          </li>
+          <li>
+            Clues left: { props.game.clue_counter }
+          </li>
+          <li>
+            Misses remaining: { props.game.miss_counter }
+          </li>
+        </ul>
         <h2>Current Players</h2>
         <ul>
           {props.game.hands.map(h => (
