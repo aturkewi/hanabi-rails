06e77e0d4bd63b6eef13dcbeace078844775e541
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-07-05 16:15:17 -0400

    Remove client dir into its own repo

--- client/src/components/NotFound/index.js
+++
@@ -1,6 +0,0 @@
-// @flow 
-import React from 'react';
-
-const NotFound = () => <div>Not Found</div>;
-
-export default NotFound;
--- client/src/services/Cable/index.js
+++
@@ -1,9 +0,0 @@
-import ActionCable from 'actioncable';
-
-export default () => {
-  const token = localStorage.token;
-  const App = {};
-  App.cable = ActionCable.createConsumer(`ws://localhost:3001/cable?token=${token}`);
-  return App.cable
-}
-
